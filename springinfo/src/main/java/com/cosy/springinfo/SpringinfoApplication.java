7530002b724281e69f7a7324dbc1c78592f49e1e
Cosy <staycosy@163.com>
2018-09-03 19:23:16 +0800

    add project

---
+++ springinfo/src/main/java/com/cosy/springinfo/SpringinfoApplication.java
@@ -0,0 +1,12 @@
+package com.cosy.springinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinfoApplication.class, args);
+	}
+}
---
+++ springinfo/src/test/java/com/cosy/springinfo/SpringinfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cosy.springinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringinfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
